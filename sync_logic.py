45e8096cb7fa2efe7d57778139cafdc5c2195a51
Eric Stein <toba@des.truct.org>
2014-09-16 19:50:20 -0400

    starting out - getting the first sync and snap to emit

--- sync_logic.py
+++ sync_logic.py
@@ -1,17 +1,120 @@
+import threading
+
+class Flags(object) :
+	"""
+	Flag constants namespace
+	"""
+
+	"""
+	Emit a sync event at the end of processing every message that brings the NetworkView into full sync.
+	"""
+	ALL_SYNCS = 1
+
+	"""
+	Emit all snapshot events, no matter the synchronization state. By default, only the initial sync snapshot is emitted,
+	and all future snapshots that are used to re-establish sync are synthesized into a synthetic pair of add/remove events as needed.
+	"""
+	ALL_SNAPSHOTS = 2
+
+class Event(object) :
+	def __init__(self, serial, nodes) :
+		self.serial = serial
+		self.nodes = nodes
+
+	def istype(self, cls) :
+		return self.__class__ == cls
+
+class Sync(Event) :
+	pass
+
+class Desync(Event) :
+	pass
+
+class Snapshot(Event) :
+	pass
+
+class Add(Event) :
+	pass
+
+class Remove(Event) :
+	pass
+
 class NetworkView(object) :
 	"""
-	
+	Accepts incoming messages transmitted by transmitter.py
+
+	Goals:
+	Create a gapless (fill any gaps with synthesized events) stream of update events
+	Event types:
+		sync
+		add
+		remove
+		desync
+
+	So far, flags are not used.
 	"""
 	
-	def __init__(self) :
+	def __init__(self, flags=0) :
 		self.subscribers = list()
-
+		self.flags = flags
+		
+		# the serial that self.net is as-of.
+		self.serial = None
+		self.synced = False
+		self.net = None
+		
 	def add_subscriber(self, s) :
 		self.subscribers.append(s)
 
-	def process_message(self.mobj) :
+	def pass_event(self, e) :
+		for s in self.subscribers :
+			s.recv_event(e)
+
+	def handle_message(self, mobj) :
 		"""
 		Process an inbound message, update any state that need be updated, and emit events to subscribers.		
 		"""
 
-		pass
+		# TODO handle errors from invalid input. Somehow.
+
+		m_ser = mobj['serial']
+		m_prev_ser = mobj.get('prev_serial')
+		parts = mobj['parts']
+		n_parts = len(parts)
+		last_part = parts[-1]
+
+		if not self.synced :
+			# desynced case
+			if last_part['type'] != 'snapshot' :
+				# if we want to emit multiple desyncs, we should do one now...
+				return
+
+			if self.net is None :
+				# first case; never had a sync yet.
+				self.serial = m_ser
+				self.synced = True
+				self.net = set(last_part['nodes'])
+				self.pass_event(Snapshot(self.serial, self.net))
+				self.pass_event(Sync(self.serial, None))
+			else :
+				# we were synced once, but aren't right now. whoops!
+				pass
+				raise RuntimeError("woops, need to implement gap correction!")
+		else :
+			# we are, for now, synchronized. check out this incoming message...
+			if (not m_prev_ser) or (m_prev_ser != self.serial) :
+				# here's the case where the previous serial doesn't match the one we have... whoops.
+				self.synced = False
+				self.pass_event(Desync(self.serial, None))
+				return
+
+			self.serial = m_ser
+			# handle no-op, add, remove.
+
+			for part in parts :
+				if part['type'] == 'add' :
+					self.net = self.net.union(set(part['nodes']))
+				elif part['type'] == 'remove' : 
+					self.net = self.net.difference(set(part(['nodes'])))
+			self.pass_event(Sync(self.serial, None))
+
--- tests/transmission_tests.py
+++ tests/transmission_tests.py
@@ -2,8 +2,44 @@ import unittest
 
 import sync_logic as sl
 
+class EventTests(unittest.TestCase) :
+	def test_istype(self) :
+		e = sl.Add(112598175, set())
+		#self.assertTrue(e.istype(sl.Add))
+		#self.assertFalse(e.istype(sl.Remove))
+
 class TestSubscriber(object) :
-	def test_empty(self) :
+	def __init__(self) :
+		self.events = list()
+
+	def recv_event(self, e) :
+		self.events.append(e)
+	
+class TestSubscription(unittest.TestCase) :
+	def setup(self) :
 		ts = TestSubscriber()
 		nv = sl.NetworkView()
 		nv.add_subscriber(ts)
+		return ts, nv
+
+	def test_empty(self) :
+		ts, nv = self.setup()
+		self.assertEquals(0, len(ts.events))
+
+	def test_snap_sync(self) :
+		ts, nv = self.setup()
+
+		nv.handle_message({
+			'serial' : 1,
+			'parts' : [
+				{
+					'type' : 'snapshot',
+					'nodes' : set(['a'])
+				}
+			]
+		})
+		snap_e = ts.events[0]
+		self.assertTrue(snap_e.istype(sl.Snapshot))
+		sync_e = ts.events[1]
+		self.assertTrue(sync_e.istype(sl.Sync))
+		self.assertEquals(len(ts.events), 2)

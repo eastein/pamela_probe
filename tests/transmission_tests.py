88657122306e5541ee564cc097082613cda32a30
Eric Stein <toba@des.truct.org>
2014-09-16 20:09:57 -0500

    test that remove removes and add adds

--- tests/transmission_tests.py
+++ tests/transmission_tests.py
@@ -20,7 +20,7 @@ INITIAL_SNAP = {
 	'parts' : [
 		{
 			'type' : 'snapshot',
-			'nodes' : set(['a'])
+			'nodes' : set(['a', 'b'])
 		}
 	]
 }	
@@ -31,6 +31,17 @@ ADD_MSG = {
 	'parts' : [
 		{
 			'type' : 'add',
+			'nodes' : set(['c'])
+		}
+	]
+}
+
+REMOVE_MSG = {
+	'serial' : 3,
+	'prev_serial' : 2,
+	'parts' : [
+		{
+			'type' : 'remove',
 			'nodes' : set(['b'])
 		}
 	]
@@ -54,14 +65,14 @@ class TestSubscription(unittest.TestCase) :
 		snap_e = ts.events[0]
 		self.assertTrue(snap_e.istype(sl.Snapshot))
 		self.assertEquals(snap_e.serial, 1)
-		self.assertEquals(snap_e.nodes, set(['a']))
+		self.assertEquals(snap_e.nodes, set(['a', 'b']))
 		sync_e = ts.events[1]
 		self.assertTrue(sync_e.istype(sl.Sync))
 		self.assertEquals(sync_e.serial, 1)
 		self.assertEquals(sync_e.nodes, None)
 		self.assertEquals(len(ts.events), 2)
 
-	def test_add(self) :
+	def test_add_remove(self) :
 		ts,nv = self.setup()
 		
 		nv.handle_message(INITIAL_SNAP)
@@ -71,3 +82,12 @@ class TestSubscription(unittest.TestCase) :
 		self.assertEquals(len(ts.events), 3)
 		addm = ts.events[2]
 		self.assertTrue(addm.istype(sl.Add))
+		self.assertEquals(addm.nodes, set(['c']))
+
+		self.assertEquals(set(['a','b','c']), nv.net)
+		nv.handle_message(REMOVE_MSG)
+		self.assertEquals(len(ts.events), 4)
+		rem = ts.events[3]
+		self.assertTrue(rem.istype(sl.Remove))
+
+		self.assertEquals(set(['a','c']), nv.net)
